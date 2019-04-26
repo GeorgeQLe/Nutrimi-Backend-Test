5590c2d1dbf77c4bdefe0c84600ee5d7500784f5
George <legeorge4@gmail.com>
2019-04-26 17:21:46 -0400

    Finished server API to Postmates communication

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask_app import app
 from sqlite import sqlite_flask
 from yelp import yelp_api
+from postmates import postmates_api
 
+app.register_blueprint(postmates_api)
 app.register_blueprint(sqlite_flask)
 app.register_blueprint(yelp_api)
 
